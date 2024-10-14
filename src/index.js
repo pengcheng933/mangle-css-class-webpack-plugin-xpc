8b16e4304af82e55d3c15e37175ada1a167558e3
pengchneg <1900965366@qq.com>
2024-10-14 21:08:53 +0800

    sace

--- src/index.js
+++ src/index.js
@@ -1,80 +1,79 @@
-const fs = require('fs')
-const path = require('path')
-const optimizer = require('./optimizer')
-const ClassGenerator = require('./classGenerator')
+const fs = require("fs");
+const path = require("path");
+const optimizer = require("./optimizer");
+const ClassGenerator = require("./classGenerator");
 
 const runner = (compiler, compilation, opts, classGenerator) => {
-  const optimize = optimizer(compiler, compilation, opts, classGenerator)
+  const optimize = optimizer(compiler, compilation, opts, classGenerator);
   return (assets, callback) => {
-    optimize(assets)
-    callback()
-  }
-}
+    optimize(assets);
+    callback();
+  };
+};
 
-let classGenerator
-const cachePath = path.resolve(
-  process.cwd(),
-  'plugins/mangle-css-class-plugin-c/cache2.json'
-)
+let classGenerator;
+const cachePath = path.resolve(__dirname, "cache.json");
+console.log(cachePath);
+
 const detectingExistenceFile = (path) => {
   return new Promise((resolve, reject) => {
     fs.access(path, fs.constants.F_OK, (err) => {
       if (err) {
         // 如果文件不存在则创建文件
-        fs.writeFile(path, '{}', (err) => {
+        fs.writeFile(path, "{}", (err) => {
           if (err) {
-            reject(err)
+            reject(err);
           } else {
-            resolve()
+            resolve();
           }
-        })
+        });
       } else {
-        resolve()
+        resolve();
       }
-    })
-  })
-}
+    });
+  });
+};
 const gettingCache = () => {
   return new Promise((resolve, reject) => {
-    fs.readFile(cachePath, 'utf8', (err, data) => {
-      if (err) reject(err)
-      resolve(JSON.parse(data))
-    })
-  })
-}
-let index = 0
+    fs.readFile(cachePath, "utf8", (err, data) => {
+      if (err) reject(err);
+      resolve(JSON.parse(data));
+    });
+  });
+};
+let index = 0;
 class Plugin {
   constructor(opts = {}) {
-    this.opts = opts
+    this.opts = opts;
   }
 
   apply(compiler) {
     compiler.hooks.compilation.tap(
-      'MangleCssClassPluginHooks',
+      "MangleCssClassPluginHooks",
       (compilation) => {
         compilation.hooks.optimizeChunkAssets.tapAsync(
-          'MangleCssClassPluginOptimizeChunkAssetsHooks',
+          "MangleCssClassPluginOptimizeChunkAssetsHooks",
           async (assets, callback) => {
             if (!classGenerator) {
-              classGenerator = new ClassGenerator()
-              await detectingExistenceFile(cachePath)
-              const cacheObj = await gettingCache()
+              classGenerator = new ClassGenerator();
+              await detectingExistenceFile(cachePath);
+              const cacheObj = await gettingCache();
               if (cacheObj?._newClassSize?.name) {
-                console.log('have cache')
+                console.log("have cache");
 
-                const unusedClass = []
+                const unusedClass = [];
                 for (const key in cacheObj) {
                   if (cacheObj[key].num === -1) {
-                    unusedClass.push(key)
+                    unusedClass.push(key);
                   } else {
-                    cacheObj[key].num = 0
+                    cacheObj[key].num = 0;
                   }
                 }
                 classGenerator.init(
                   cacheObj,
                   cacheObj._newClassSize.name,
                   unusedClass
-                )
+                );
               }
             }
             runner(
@@ -82,41 +81,41 @@ class Plugin {
               compilation,
               this.opts,
               classGenerator
-            )(assets, callback)
+            )(assets, callback);
           }
-        )
+        );
       }
-    )
+    );
     compiler.hooks.afterEmit.tapAsync(
-      'SaveDataPlugin',
+      "SaveDataPlugin",
       (compilation, callback) => {
         if (!index) {
-          index++
-          callback()
-          return
+          index++;
+          callback();
+          return;
         }
-        const cacheObj = classGenerator.newClassMap
+        const cacheObj = classGenerator.newClassMap;
         for (const key in cacheObj) {
           if (cacheObj[key].num <= 0) {
-            cacheObj[key].num = -1
+            cacheObj[key].num = -1;
           }
         }
         cacheObj._newClassSize = {
           name: classGenerator.newClassSize,
-          num: 1
-        }
-        fs.writeFile(cachePath, JSON.stringify(cacheObj), 'utf8', (err) => {
+          num: 1,
+        };
+        fs.writeFile(cachePath, JSON.stringify(cacheObj), "utf8", (err) => {
           if (err) {
-            console.log(err)
+            console.log(err);
           }
-        })
-        callback()
+        });
+        callback();
       }
-    )
+    );
   }
 }
 
-module.exports = Plugin
+module.exports = Plugin;
 
 /**
  * 每次打包获取缓存对象, /cache.json 格式{ 类名: { name: 'xx', num: 'xx'} }
